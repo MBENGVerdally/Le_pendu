f11a639881f3488854a8002c0b7d9862b803a0ad
MBENGVerdally <verdally.mbeng.pro@gmail.com>
2024-09-16 11:13:18 +0200

    Premère partie

--- Pendu.py
+++ Pendu.py
@@ -1,1 +1,16 @@
 ## Le pendu
+
+
+import random
+def randomaccesword():
+
+    wordlist = ["banana", "strawberry", "kiwi"]
+    wordchose = random.choice(wordlist)
+    return wordchose
+word = randomaccesword()
+print(word)
+
+guessword =  random.choice(wordlist)
+maskedword = " _ " *
+len (guessword)
+print(guessword)
